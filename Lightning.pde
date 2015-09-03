a360a1bfc25a21de6fcfc5c7263524595c731a3f
eryup <eric.yup1@gmail.com>
2015-09-03 11:34:22 -0700

    finish lightning knee

--- Lightning.pde
+++ Lightning.pde
@@ -1,36 +1,45 @@
-int startX = 0;
-int startY= 150;
+PImage photo;
+int wipe = 0;
+int shift = 20;
+int startX = 515;
+int startY= 215;
 int endX= 0;
-int endY= 150;
+int endY= 215;
 void setup()
 {
-  size(300,300);
+  size(800,450);
   background(50,10);
   strokeWeight(2);
   frameRate(30);
   colorChange();
+  photo = loadImage("knee.jpg");
+  image(photo,0,0);
 }
 void draw()
 {
-while (endX <= 300) {
-	endX=startX+(int)(Math.random()*10);
-	endY=startY+(int)(Math.random()*19)-9;
-	line(startX,startY,endX,endY);
-	colorChange();
-	startX=endX;
-	startY=endY;
-}
-//	  fill(0,130,130,30);
-  //rect(0, 0, 300, 300);
-}
-void mousePressed()
-{
-startX = 0;
-startY= 150;
+	if (wipe>500){
+		 image(photo,0,0);
+		 wipe=0;
+	}
+	 lightning();
+  startX = 515;
+startY= 215;
 endX= 0;
-endY= 150;
+endY= 215;
 }
 void colorChange()
 {
 	stroke((int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255));
+}
+void lightning()
+{
+	while (endX <= 800) {
+	endX=startX+(int)(Math.random()*(shift));
+	endY=startY+(int)(Math.random()*(shift*2))-(shift);
+	line(startX,startY,endX,endY);
+	colorChange();
+	startX=endX;
+	startY=endY;
+	wipe++;
+}
 }
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,42 +14,50 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
-int startX = 0;
-int startY= 150;
+PImage photo;
+int wipe = 0;
+int shift = 20;
+int startX = 515;
+int startY= 215;
 int endX= 0;
-int endY= 150;
+int endY= 215;
 public void setup()
 {
-  size(300,300);
+  size(800,450);
   background(50,10);
   strokeWeight(2);
   frameRate(30);
   colorChange();
+  photo = loadImage("knee.jpg");
+  image(photo,0,0);
 }
 public void draw()
 {
-while (endX <= 300) {
-	endX=startX+(int)(Math.random()*10);
-	endY=startY+(int)(Math.random()*19)-9;
-	line(startX,startY,endX,endY);
-	colorChange();
-	startX=endX;
-	startY=endY;
-}
-//	  fill(0,130,130,30);
-  //rect(0, 0, 300, 300);
-  background(50,10);
-}
-public void mousePressed()
-{
-startX = 0;
-startY= 150;
+	if (wipe>500){
+		 image(photo,0,0);
+		 wipe=0;
+	}
+	 lightning();
+  startX = 515;
+startY= 215;
 endX= 0;
-endY= 150;
+endY= 215;
 }
 public void colorChange()
 {
 	stroke((int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255));
+}
+public void lightning()
+{
+	while (endX <= 800) {
+	endX=startX+(int)(Math.random()*(shift));
+	endY=startY+(int)(Math.random()*(shift*2))-(shift);
+	line(startX,startY,endX,endY);
+	colorChange();
+	startX=endX;
+	startY=endY;
+	wipe++;
+}
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Lightning" };
